2d630314661be3bc4b6bbcec35bd00acab9dbb99
Jomar Cardoza <jmraccx@gmail.com>
2022-08-04 20:44:19 -0400

    crossorigin to crossOrigin

--- pages/_document.js
+++ pages/_document.js
@@ -16,14 +16,14 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap"
             rel="stylesheet"
           />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap"
             rel="stylesheet"
