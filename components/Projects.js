9bc83c799921c1f376ec3c639ee27070844eb3c3
Jomar Cardoza <jmraccx@gmail.com>
2022-08-07 20:50:57 -0400

    styled and added github links to all projects

--- components/Projects.js
+++ components/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiOutlineLink } from "react-icons/ai";
+import { AiOutlineLink, AiOutlineGithub } from "react-icons/ai";
 import { TbBrandTailwind } from "react-icons/tb";
 import { IoLogoReact } from "react-icons/io";
 
@@ -13,6 +13,7 @@ function Projects() {
       projectName: "Rest Countries API",
       desc: "An application that allows a user to filter through regions and indivual countries to return some information on the selected country",
       link: "https://rest-countries-nextjs-zeta.vercel.app/",
+      git: "https://github.com/jmrc1995/Rest-Countries-NEXTJS/tree/main",
     },
 
     {
@@ -20,11 +21,13 @@ function Projects() {
       desc: "An application that allows an authenticated spotify user to filter through their 20 most played tracks",
       disc: "User access may be limited, click here for more info",
       link: "https://jmrc1995.github.io/goodsoup/",
+      git: "https://github.com/jmrc1995/goodsoup",
     },
     {
       projectName: "Todo",
       desc: "A simple task managing application.",
       link: "https://jmrc1995.github.io/Todo-App-JMRC/",
+      git: "https://github.com/jmrc1995/Todo-App-JMRC/tree/master",
     },
   ];
 
@@ -33,11 +36,13 @@ function Projects() {
       projectName: "Greensia Earthworks Landing Page",
       desc: "Landing page",
       link: "https://jmrc1995.github.io/GEW.plp/",
+      git: "https://github.com/jmrc1995/GEW.plp",
     },
     {
       projectName: "Room-Homepage",
       desc: "Landing page",
       link: "https://jmrc1995.github.io/Room-Homepage-Master/",
+      git: "https://github.com/jmrc1995/Room-Homepage-Master",
     },
   ];
 
@@ -51,9 +56,16 @@ function Projects() {
           {" "}
           {projects.map((project) => (
             <li className="mt-5 md:mt-0 " key={project.projectName}>
-              <a href={project.link} className="font-dmserif text-lg flex ">
-                {project.projectName}<AiOutlineLink size={20} className="hover:text-orange ml-2"/> 
-              </a>
+              <div className="flex">
+                <a href={project.link} className="font-dmserif text-lg flex items-center">
+                  {project.projectName}
+                  <AiOutlineLink size={20} className="hover:text-orange ml-2" />
+                </a>
+                <a href={project.git} className="font-dmserif text-lg flex items-center ">
+                  <AiOutlineGithub className="hover:text-orange ml-1" />
+                </a>
+              </div>
+
               <p className="font-opensans text-sm"> {project.desc}</p>
             </li>
           ))}
@@ -68,11 +80,19 @@ function Projects() {
           {" "}
           {designs.map((design) => (
             <li className="mt-5 md:mt-0" key={design.projectName}>
-            
-              <a href={design.link} className=" font-dmserif text-lg flex items-center">
-                {design.projectName} <AiOutlineLink size={20} className="hover:text-orange ml-2"/> 
-                
-              </a>
+              <div className="flex">
+                <a
+                  href={design.link}
+                  className=" font-dmserif text-lg items-center flex"
+                >
+                  {design.projectName}{" "}
+                  <AiOutlineLink size={20} className="hover:text-orange ml-2" />
+                </a>
+                <a href={design.git} className="font-dmserif text-lg flex items-center  ">
+                  <AiOutlineGithub className="hover:text-orange ml-1" />
+                </a>
+              </div>
+
               <p className="text-sm font-opensans"> {design.desc}</p>
             </li>
           ))}
